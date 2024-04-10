00d65c571ff4357681a95fb7712e9399d2a3f41b
SManush <manushsimonyan@gmail.com>
2024-04-10 21:22:36 +0400

    - Spawning pieces. - Position settings.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,7 +1,44 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; } //get reference to Piece
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
+    //spawn our peacies
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>(); //assigne that
 
+        for (int i = 0; i < this.tetrominos.Length; i++)
+        {
+            this.tetrominos[i].Initialise();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominos.Length);
+        TetrominoData data = this.tetrominos[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data); //initialize tha piece
+        Set(this.activePiece);
+    }
+    
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 }
