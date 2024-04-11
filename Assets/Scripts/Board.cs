5408fe8c608af219edbde99f5c05c15c4e78471e
SManush <manushsimonyan@gmail.com>
2024-04-11 10:50:22 +0400

    - Movement.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,6 +7,17 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; } //get reference to Piece
     public TetrominoData[] tetrominos;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
+
     //spawn our peacies
     private void Awake()
     {
@@ -40,5 +51,37 @@ public class Board : MonoBehaviour
             Vector3Int tilePosition = piece.cells[i] + piece.position;
             this.tilemap.SetTile(tilePosition, piece.data.tile);
         }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(position))
+            {
+                return false;
+            }
+
+        }
+
+        return true;
     }
 }
